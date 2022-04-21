2506f5be275a4a1af3d57013e688d779d36552b7
D-Maxwell <98100481+D-Maxwell@users.noreply.github.com>
2022-04-21 22:13:17 +0200

    Update colovaria.py
    removed unnecessary prints

--- colovaria.py
+++ colovaria.py
@@ -21,16 +21,16 @@ def colovaria():
     World = np.array([[0,0,1,1],[1,0,1,0],[0,0,1,1],[1,0,0,1]])
     #print(World)
     for cx in range(len(World)):
+        # watch out for this: 'cx in World' gives cx the value of the currently targeted element, whereas 'cx in range(len(World))' gives cx the value of the index of the currently targeted element, so that it acts as "it should", or rather as "I expect it to". This way cx refers to the number of iterations, and World[cx] returns the value corresponding to this index. (also 'len()' is used, I get why but I'll do some testing to see what happens without it, someday I'll do it, I swear)                   *no*
         for cy in range(len(World[cx])):
             #if World[0][cx] == 1:
             #print(int(x/12), int(y/12), cx, cy)
-            print(cx+10,cy+10)
+            #print(cx+10,cy+10)
             if World[cx][cy] == 1:
                 gol.create_rectangle(cx*10,cy*10,cx*10+10,cy*10+10, fill="white")
-                # create_rectangle(x1,y1,x2,y2,clr) -> drawRect(pos[x,y],size[w,h],clr[r,g,b])
-                print("\-> yes")
-            else:
-                print("\-> no")
+                #print("\-> yes")
+            #else:
+                #print("\-> no")
             #y += 10 + 2
         #y = 0
         #x += 10 + 2
