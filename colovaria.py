d05221ebd5b6b9ba138ad211ba98ec3e29b3009e
D-Maxwell <98100481+D-Maxwell@users.noreply.github.com>
2022-04-26 13:51:34 +0200

    Add files via upload

--- colovaria.py
+++ colovaria.py
@@ -1,5 +1,6 @@
 ### co-loh-VA-ree-ah!
 
+import lumos
 import tkinter as tk
 import tkinter.ttk as ttk
 import numpy as np
@@ -12,14 +13,17 @@ win.geometry("800x600")
 win.title("Welland's Game of Life")
 win.configure(bg="#d1d2d1", padx=25, pady=25)
 
+CamX = 0
+CamY = 0
+CamSpeed = 4
 
-
-gol = tk.Canvas(width=400,height=400, highlightthickness=0, bg="black") #bd=-2
+gol = tk.Canvas(win, width=400,height=400, highlightthickness=0, bg="black") #bd=-2
 
 def colovaria():
     #x, y = 0, 0
-    World = np.array([[0,0,1,1],[1,0,1,0],[0,0,1,1],[1,0,0,1]])
+    #World = np.array([[0,0,1,1],[1,0,1,0],[0,0,1,1],[1,0,0,1]])
     #print(World)
+    World = lumos.lumos(8,8)
     for cx in range(len(World)):
         # watch out for this: 'cx in World' gives cx the value of the currently targeted element, whereas 'cx in range(len(World))' gives cx the value of the index of the currently targeted element, so that it acts as "it should", or rather as "I expect it to". This way cx refers to the number of iterations, and World[cx] returns the value corresponding to this index. (also 'len()' is used, I get why but I'll do some testing to see what happens without it, someday I'll do it, I swear)                   *no*
         for cy in range(len(World[cx])):
@@ -38,19 +42,25 @@ def colovaria():
 
 gol.pack()
 
-#scrll = tk.Scrollbar(orient="horizontal", width=400)
-#gol.xview_scroll(20,"units")
-#scrll.pack()
-#gol.xview_scroll(100, "units")
+
+tools = ttk.Frame(win, padding=4)
+
+move = tk.Frame(tools)
+#tools = tk.Frame(win, pady=4, bg="")
+btn_left = ttk.Button(move, text="<-", width=8).pack(side="left")
+btn_up = ttk.Button(move, text="^^", width=8).pack(side="left")
+btn_down = ttk.Button(move, text="vv", width=8, command="print(CamX)").pack(side="left")
+btn_right = ttk.Button(move, text="->", width=8, command="CamX+=CamSpeed").pack(side="left")
+move.pack(side="left")
+
+zoom = tk.Frame(tools)
+btn_in = ttk.Button(move, text="+", width=8).pack(side="left")
+btn_out = ttk.Button(move, text="-", width=8).pack(side="left")
+zoom.pack(side="right")
+
+tools.pack()
 
 
-
-#main = tk.Frame(bg="#d1d2d1", width=win.winfo_width(), height=win.winfo_height()).pack()
-#def info():
-    #print(win.winfo_width(), win.winfo_height())
-
-#hw = tk.Label(text="Hello, world!").pack()
-#pressMe = ttk.Button(text="Press Me!", command=info).pack()
 
 colovaria()
 
